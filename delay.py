83c7a239ce802fe38fda7bbe0ad081e1925b1044
gf0842wf <gf0842wf@gmail.com>
2014-07-10 20:50:22 +0800

    utils: timeout
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- delay.py
+++ delay.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """以协程的方式实现:延时调用,循环调用,超时调用
-: 如果为了优化协程数,最好使用gevent.Timeout, 而不是使用这里的TimeoutMixin
+: 如果为了优化协程数,最好使用 gevent.Timeout(下面的 test_timeout), 而不是使用这里的 TimeoutMixin
 """
 
 from gevent import Greenlet
@@ -70,6 +70,31 @@ class LoopingCall(Greenlet):
             self.delay.call()
 
 
+class Timeout(gevent.Timeout):
+    """和gevent.Timeout 相比增加了已经过去的时间和剩余时间属性"""
+    
+    def __init__(self, seconds=None, exception=None):
+        gevent.Timeout.__init__(self, seconds, exception)
+        self.stime = None
+    
+    def start(self):
+        self.stime = time.time()
+        gevent.Timeout.start(self)
+    
+    @property
+    def passed(self):
+        if self.stime is None: return 0
+        now = time.time()
+        return now - self.stime
+    
+    @property
+    def rest(self):
+        if self.stime is None: return 0
+        if self.seconds is None: return 0
+        now = time.time()
+        return self.seconds - (now - self.stime)
+    
+
 class TimeoutMixin(object):
     """以一个微线程的方式实现一个超时调用:
     class Test(TimeoutMixin):
@@ -109,6 +134,27 @@ class TimeoutMixin(object):
         self.dc.start()
         
         
+def test_timeout(timeout=None):
+    """这个是代码片段(不可执行), 示例使用gevent.Timeout 来处理收消息超时"""
+    timer = None
+    if timeout is not None:
+        timer = gevent.Timeout(timeout)
+        timer.start()
+    try:
+        data = self.sock.recv(self.buffer_size)
+        if not data: return
+    except gevent.Timeout, t:
+        assert timer == t, t
+        print "Socket: HeatBeat Timeout!"
+        return
+    except socket.error, e:
+        print "Socketrecv error:", e
+        return
+    finally:
+        if timer:
+            timer.cancel()
+            
+            
 if __name__ == "__main__":
     
     class TestTimeout(TimeoutMixin):
