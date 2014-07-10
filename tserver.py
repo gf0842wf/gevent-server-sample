f746919572637544f77463fceafcf998010af271
gf0842wf <gf0842wf@gmail.com>
2014-07-10 18:41:23 +0800

    tcp server sample
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- tserver.py
+++ tserver.py
@@ -4,41 +4,42 @@
 
 import gevent
 from gevent.server import StreamServer
-from endpoint import EndPoint
+from tendpoint import EndPoint
 
 
-class Observer(EndPoint):
+class Bot(EndPoint):
     
     def on_connection_closed(self):
-        print("Observer {0} closed the connecton".format(id(self)))
+        print("Bot {0} closed the connecton".format(id(self)))
 
     def on_connection_lost(self):
-        print("Observer {0} lost".format(id(self)))
+        print("Bot {0} lost".format(id(self)))
 
     def on_data(self, data):
-        pass
+        print '%s: got %r ' % (self, data)
+        self.put_data(data)
 
 
-class ObserverManager(gevent.Greenlet):
+class TCPManager(gevent.Greenlet):
     
     def __init__(self, port):
         self.port = port
         gevent.Greenlet.__init__(self)
 
-    def _connection_handler(self, client, address):
-        # 这里 client 是socket
+    def _connection_handler(self, trans, address):
+        # 这里 trans 是socket
         print("New Connection From {0}".format(address))
-        observer = Observer(client)
-        observer.start()
+        bot = Bot(trans, address)
+        bot.start()
 
     def _run(self):
-        print("TCP Observer Listen at port {0}".format(self.port))
+        print("TCP server Listen at port {0}".format(self.port))
         server = StreamServer(('0.0.0.0', self.port), self._connection_handler)
         server.serve_forever()
 
 
 if __name__ == "__main__":
-    ob = ObserverManager(7000) # 这个是通过协程执行的,所以下面需要wait等待
+    ob = TCPManager(7000) # 这个是通过协程执行的,所以下面需要wait等待
     ob.start()
     gevent.wait()
     
