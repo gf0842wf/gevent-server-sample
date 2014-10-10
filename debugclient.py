c4cf2644c995755ef01c5f92853a953b18c1ef8b
gf0842wf <gf0842wf@gmail.com>
2014-10-10 12:05:00 +0800

    m
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- debugclient.py
+++ debugclient.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8- -*-
  
 import socket
-import sys
- 
+import sys, os
+
+ARGS = filter(lambda arg: isinstance(arg, list) and len(arg)==2, 
+              [arg.lstrip("-").split("=") for arg in sys.argv[1:]])
+ARGS = dict(ARGS)
+
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error as e:
     print repr(e)
  
-remote_addr = ("127.0.0.1", 7006)
+remote_addr = (ARGS["host"], int(ARGS["port"]))
 sock.connect(remote_addr)         
 
 f = sock.makefile("r")
 while True:
     s = raw_input(">>> ")
     sock.sendall(s+"\n")
-    print f.readline()
+    print sock.recv(4196).rstrip("\n") # 简单接收了,没做分包粘包处理
  
 sock.close()
+
+# python debugclient.py --host=127.0.0.1 --port=8106
--- debugserver.py
+++ debugserver.py
@@ -1,49 +1,68 @@
 # -*- coding: utf-8 -*-
 
-"""部署服务时,debug服务用来交互式修改主程序运行时数据,类似twisted的ssh服务"""
+"""部署服务时,debug服务用来交互式修改主程序运行时数据,类似ssh连到服务器,用python进行交互操作"""
+"""危险声明: 一旦掌握此权限,可以对程序进行任何查看和修改!!!最好是通过ssh跳转来操作(debugserver端口不对外开放,在服务器执行debugclient来连接)"""
 
-# prefix = ">>> "
+# 客户端交换命令如下
+# 执行语句---需要加上exec
 # exec "from time import sleep"
 # exec "sleep(3)"
 # exec "d={'a':4}"
 # exec "d['a'] = 5"
-# exec "print d"
+
+# 执行表达式---直接运行表达式
+# addr
+
+# 执行print---直接print 表达式--这个可以不用,直接用执行表达式即可打印出来
+# print addr <==> addr
+
 
 import socket
+import threading
+import logging
  
-fd_map = {} # fd到socket的映射, 记住服务器socket和连接进来的客户端socket,可以向指定连接通信
- 
-# 建立socket对象
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    fd = sock.fileno()
-    fd_map[fd] = sock
-except socket.error as e:
-    print repr(e)
- 
-# 设置socket选项 这里设置SO_REUSEADDR含义:当socket意外关闭,马上释放占有的端口
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
- 
-# 绑定到某个端口
-host_addr = ("0.0.0.0", 7006)
-sock.bind(host_addr)
- 
-sock.listen(5)
- 
-while True:
-    # 接受连接 accept返回连接的socket对象和addr
-    conn, addr = sock.accept()
-    fd_map[fd] = conn
+def start_server(host_addr=("0.0.0.0", 7006)):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        logging.error(repr(e))
+     
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+     
+    sock.bind(host_addr)
+    logging.info("debugserver start @ %s" % str(host_addr))
+     
+    sock.listen(5)
+    
+    while True:
+        conn, addr = sock.accept()
+        threading.Thread(target=connection_handler, args=(conn, addr)).start()
+    
+def connection_handler(conn, addr):
+    logging.info("client %s connected" % str(addr))
     f = conn.makefile()
     while True:
-        cmd = f.readline().rstrip("\n")
         try:
-            result = eval(cmd)
-        except:
-            result = "" # globals()
-#             exec cmd
-        print repr(cmd)
-        conn.sendall("%s\nab\n" % result)
- 
-# 关闭socket
-sock.close()
+            cmd = f.readline().rstrip("\n")
+        except Exception as e:
+            logging.info("client %s closed | %s" % (str(addr), e))
+            break
+        if cmd.startswith("exec"):
+            try:
+                exec cmd
+                result = ""
+            except Exception as e:
+                result = "%s: %s" % (e.__class__, str(e))
+        else:
+            if cmd.startswith("print"):
+                cmd = cmd[5:].lstrip()
+            try:
+                result = eval(cmd)
+            except Exception as e:
+                result = "%s: %s" % (e.__class__, str(e))
+        conn.sendall("%s\n" % str(result))
+    conn.close()
+        
+
+if __name__ == "__main__":
+    start_server(("0.0.0.0", 8106))
